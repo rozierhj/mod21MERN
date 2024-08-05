8dbb72037df932a32aa00d7962bb57340c6d52f9
rozierhj <160989130+rozierhj@users.noreply.github.com>
2024-08-04 20:38:05 -0400

    temporarily added build folder to the client to stop code error

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
+//removed /build
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
