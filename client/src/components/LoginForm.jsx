d4de06867725a97570ea04115f23827a3d10c48d
rozierhj <160989130+rozierhj@users.noreply.github.com>
2024-08-09 14:36:24 -0400

    users can now be logged in again using MERN setup. now working on getting them to their saved books

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,11 +1,16 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-import { loginUser } from '../utils/API';
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
+
+  const [ loginUser, {error} ] = useMutation(LOGIN_USER);
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,16 +31,22 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      // const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      const {data} = await loginUser({
+        variables: {...userFormData},
+      });
+
+      Auth.login(data.login.token);
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
+    //   const { token, user } = await response.json();
+    //   console.log(user);
+    //   Auth.login(token);
+    // }
+     catch (err) {
       console.error(err);
       setShowAlert(true);
     }
